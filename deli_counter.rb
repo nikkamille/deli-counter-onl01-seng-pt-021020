0e66e600a361ed8e6d45355cd229c0032fd01330
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-20 23:45:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    array.each_with_index {|name, index| 
+    with_numbers = array.each_with_index {|name, index| 
     
     # puts "The line is currently: #{index + 1}. #{name}"}
   end
