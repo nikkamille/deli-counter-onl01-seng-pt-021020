85c756258e13e56c1579cdb61905e5cbcbd18f40
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-21 00:03:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
     puts "The line is currently empty."
   else
     phrase = "The line is currently:"
-    with_numbers = array.map.with_index {|name, index| "#{index + 1}. #{name}}
+    with_numbers = array.map.with_index {|name, index| "#{index + 1}. #{name}"}
     puts phrase + with_numbers.join(" ")
   end
 end
