af89d75d50e1c7eb0eb651753454e803aee1190b
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-21 01:30:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else
     # phrase = "Currently serving "
-    array.shift {|name| puts "Currently serving #{name}."} # && array.delete_at(0)
+    array.each {|name| puts "Currently serving #{name}."} # && array.delete_at(0)
     
     # array.delete_at(0)
   end
