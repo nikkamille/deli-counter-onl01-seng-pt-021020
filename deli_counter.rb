64f78b9d55b0c803a002b7da3486365595ff8588
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-21 01:06:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,4 +10,7 @@ end
 
 def take_a_number(array, name)
   # expect "Welcome, Ada. You are number 1 in line."
+  array = []
+  array << name
+  puts "Welcome, #{name}. You are number #{array.count + 1} in line."
 end
